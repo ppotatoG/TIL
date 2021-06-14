f6bb2b8c89e57c47a53fde0ea6c57df8fcc2e84e
ppotatoG <thgus2270@gmail.com>
2021-06-14 14:36:56 +0900

    a < b 일 때 모든 수의 합

--- programmers/12912/README.md
+++ programmers/12912/README.md
@@ -96,3 +96,24 @@ solution(3, 5)
 solution(5, 3)
 
 ```
+<br>
+
+문제 상관 없이 모든 수를 더하는 함수
+```
+function solution(a, b){
+
+    let answer = 0
+    let Arr = []
+    // Arr.push(a, b)
+
+    for(let i = a ; i <= b; i ++){
+        Arr.push(i)
+        console.log(Arr)
+        answer = Math.abs(Arr.reduce((a, b) => (a + b))) 
+    }
+    // return answer;
+    console.log(answer)
+
+}
+solution(1, 10)
+```
--- programmers/12912/js.js
+++ programmers/12912/js.js
@@ -2,18 +2,15 @@ function solution(a, b){
 
     let answer = 0
     let Arr = []
-    Arr.push(a, b)
+    // Arr.push(a, b)
 
-    if (a == b) answer = a
-    else {
-        for(let i = Arr[0] ; i < Arr[1]; i ++){
-            answer = Math.abs(Arr.reduce((a, b) => (a + b))) 
-            answer += i
-        }
+    for(let i = a ; i <= b; i ++){
+        Arr.push(i)
+        console.log(Arr)
+        answer = Math.abs(Arr.reduce((a, b) => (a + b)))
     }
     // return answer;
     console.log(answer)
 
 }
-solution(3, 5)
 solution(5, 3)
