2f24180158c40043ae102af0e2abd2000065aa1d
ppotatoG <thgus2270@gmail.com>
2021-06-30 17:53:32 +0900

    split 내용 정리

---
+++ js/string/split/js.js
@@ -0,0 +1,12 @@
+let s = '만천하의 새 이는 가슴에 그러므로 들어 것이다.'
+
+let first = s.charAt(0)
+let last = s.charAt(s.length - 1)
+
+let all = ''
+
+for(let i = 0; i <s.length; i++) all += s.charAt(i)
+
+console.log(first)
+console.log(last)
+console.log(all)
