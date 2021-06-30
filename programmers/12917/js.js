1da29bf5e664a11fccfcdffe2aae3ae3e4be29e4
ppotatoG <thgus2270@gmail.com>
2021-06-30 17:53:47 +0900

    .

---
+++ programmers/12917/js.js
@@ -0,0 +1,12 @@
+function solution(string) {
+
+    let answer = string.split("").filter((a, b) => a- b)
+    console.log(string.split(""))
+    console.log(answer)
+
+}
+
+
+let a = "Zbcdefg";
+
+solution(a)
