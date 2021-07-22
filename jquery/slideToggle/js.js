561cf903ba4aaa237c45c57b9e8cd692af675f63
ppotatoG <thgus2270@gmail.com>
2021-07-22 21:09:13 +0900

    블로그 다시 시작

---
+++ jquery/slideToggle/js.js
@@ -0,0 +1,96 @@
+const button = document.querySelector('button');
+const div = document.querySelector('div');
+
+button.addEventListener('click', function(){
+    console.log(div.clientHeight);
+
+    const divHeight = div.clientHeight;
+    for( i = divHeight ; i > 0; i--){
+        setTimeout(() => {
+            div.style.height = i+'px';
+        
+            console.log(i);
+        }, 1000);
+
+        console.log(i)
+    }
+    
+})
+
+// https://jeongah-story.tistory.com/162
+
+let slideUp = (target, duration=500) => {
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.boxSizing = 'border-box';
+    target.style.height = target.offsetHeight + 'px';
+    target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    window.setTimeout( () => {
+      target.style.display = 'none';
+      target.style.removeProperty('height');
+      target.style.removeProperty('padding-top');
+      target.style.removeProperty('padding-bottom');
+      target.style.removeProperty('margin-top');
+      target.style.removeProperty('margin-bottom');
+      target.style.removeProperty('overflow');
+      target.style.removeProperty('transition-duration');
+      target.style.removeProperty('transition-property');
+      //alert("!");
+    }, duration);
+  }
+let slideDown = (target, duration=500) => {
+    target.style.removeProperty('display');
+    let display = window.getComputedStyle(target).display;
+
+    if (display === 'none')
+      display = 'block';
+
+    target.style.display = display;
+    let height = target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    target.offsetHeight;
+    target.style.boxSizing = 'border-box';
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = height + 'px';
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    window.setTimeout( () => {
+      target.style.removeProperty('height');
+      target.style.removeProperty('overflow');
+      target.style.removeProperty('transition-duration');
+      target.style.removeProperty('transition-property');
+    }, duration);
+  }
+let slideToggle = (target, duration = 500) => {
+  if (window.getComputedStyle(target).display === 'none') {
+    return slideDown(target, duration);
+  } else {
+    return slideUp(target, duration);
+  }
+}
+   
+// ====  
+  
+let speedAnimation = 400;
+let targetId = document.getElementById("target");
+
+let slideBtnClick = (cl, sl) => 
+document.querySelector(cl).addEventListener('click', () => sl(targetId, speedAnimation));
+
+
+
+slideBtnClick('.triggerToggle', slideToggle);
