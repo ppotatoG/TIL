344bddcce58ea745b282c1b14521e512c721c041
ppotatoG <thgus2270@gmail.com>
2021-07-30 17:58:32 +0900

    일단 정답

--- daily/0730/README.md
+++ daily/0730/README.md
@@ -16,3 +16,19 @@ return answer
 
 
 substring에서 두번째로 들어오는 매개변수 왜 저런지 잘 모르겠다 무슨 생각으로 저렇게 했을까
+
+[12910](https://github.com/ppotatoG/TIL/tree/master/programmers/12910)
+
+```js
+let answer = []
+arr.forEach(item => {
+    if(item % divisor == 0) {
+        answer.push(item);
+        answer = answer.sort((a, b) => a - b);
+    }
+});
+
+if(answer.length == 0) answer.push(-1);
+
+return answer;
+```
--- daily/0730/js.js
+++ daily/0730/js.js
@@ -6,5 +6,34 @@ function solution(s) {
 const c1 = "abcde";
 const c2 = "qwer";
 
-console.log(solution(c1));
-console.log(solution(c2));
+// console.log(solution(c1));
+// console.log(solution(c2));
+
+//
+
+function solution2(arr, divisor) {
+    let answer = []
+    arr.forEach(item => {
+        if(item % divisor == 0) {
+            answer.push(item);
+            answer = answer.sort((a, b) => a - b);
+        }
+    });
+
+    if(answer.length == 0) answer.push(-1);
+
+    return answer;
+}
+
+const arr1 = [5, 9, 7, 10];
+const divisor1 = 5;
+
+const arr2 = [2, 36, 1, 3];
+const divisor2 = 1;
+
+const arr3 = [3, 2, 6];
+const divisor3 = 10;
+
+console.log(solution2(arr1, divisor1))
+console.log(solution2(arr2, divisor2))
+console.log(solution2(arr3, divisor3))
