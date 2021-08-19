129ea6ae74ba28d1ddd3db740d5cf5e0367edf06
ppotatoG <thgus2270@gmail.com>
2021-08-19 11:42:52 +0900

    배열 삭제

--- programmers/77884/README.md
+++ programmers/77884/README.md
@@ -4,4 +4,31 @@
 
 ### 제출한 정답
 ```js
+function solution(min, max) {
+    let arr = [];
+    let answer = 0;
+
+    for(let i = min; i < max + 1; i++ ){
+        arr.push(i);
+    }
+
+    for(let i = 0; i < arr.length; i++){
+        Number.isInteger(Math.sqrt(arr[i])) ? answer = answer - arr[i] : answer = answer + arr[i]
+    }
+
+    return answer;
+}
+```
+
+min, max를 배열로 만들지 않아도 i로 받아와서 answer에 더하면 된다!
+```js
+function solution(min, max) {
+    let answer = 0;
+    
+    for(let i = min ; i < max + 1; i++){
+        Number.isInteger(Math.sqrt(i)) ? answer = answer - i : answer = answer + i;
+    }
+
+    return answer;
+}
 ```
--- programmers/77884/js.js
+++ programmers/77884/js.js
@@ -1,19 +1,10 @@
 function solution(min, max) {
-    let arr = [];
-    let minus = [];
-
-    for(let i = min; i < max + 1; i++ ){
-        arr.push(i);
+    let answer = 0;
+    
+    for(let i = min ; i < max + 1; i++){
+        Number.isInteger(Math.sqrt(i)) ? answer = answer - i : answer = answer + i;
     }
 
-    for(let i = 0; i < arr.length; i++){
-        if(Number.isInteger(Math.sqrt(arr[i]))) {
-            minus.push(arr[i])
-            arr.splice(i, 1)
-            i --;
-        }
-    }
-
-    return (arr.reduce((a, b) => a + b)) - minus.reduce((a, b) => a + b);
+    return answer;
 }
 console.log(solution(13, 17))
