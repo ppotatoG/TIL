8c538c6a85f4fa43849a751aef5a73b39243c675
ppotatoG <thgus2270@gmail.com>
2021-08-28 14:40:09 +0900

    .

--- programmers/12935/README.md
+++ programmers/12935/README.md
@@ -11,3 +11,22 @@
 근데 if문 내에서 `num[i] == num.sort()[0]` 해주면 원본배열이 정렬되벌임ㅠㅠ
 
 let arr = num 해놓고 arr.sort()를 해줘도... 그렇게 된다....
+
+`let min = [...num].sort()[0];` 이렇게 하니까 돼따..!!
+
+<br>
+
+이렇게 하니 테스트 2, 5, 6 에서 에러ㅜㅜ
+```js
+function solution(num) {
+    let min = [...num].sort()[0];
+
+    for(let i = 0; i < num.length; i++){
+        if(num[i] == min) num.splice(i, 1)
+    }
+
+    if(num.length == 0) num = [-1];
+
+    return num;
+}
+```
--- programmers/12935/js.js
+++ programmers/12935/js.js
@@ -1,20 +1,15 @@
 function solution(num) {
-    let min = num;
+    let min = [...num].sort()[0];
 
-    console.log(min, num);
-
-    min.sort()[0];
-
-    console.log(min, num);
-
+    console.log(Math.min(num))
     for(let i = 0; i < num.length; i++){
         if(num[i] == min) num.splice(i, 1)
     }
 
-    if(num.length == 1) num = [-1];
+    if(num.length == 0) num = [-1];
 
-    // return num;
+    return num;
 }
 
-console.log(solution([4,3,2,1]));
+console.log(solution( [3,7,1,5]));
 console.log(solution([10]));
