5b0eed690b7522f532502143556b2b7ea9466365
ppotatoG <thgus2270@gmail.com>
2021-09-07 22:57:15 +0900

    채점에서 실패

--- programmers/85002/README.md
+++ programmers/85002/README.md
@@ -37,3 +37,54 @@ function solution(weights, h2h){
     return count;
 }
 ```
+
+뭐.. 어떻게 실행은 맞았는데 채점은 두개밖에 못맞았다'ㅁ';
+
+그래도.. 푼게 어디냐..!
+
+혹시 몰라서 질문 들어가서 보는데 내 코드만 유독 길당 첨부터 다시 해바야지 ㅎㅋ
+```js
+function solution(weights, h2h){
+    const count = [...weights].map((i) => i = 0);
+
+    for(let i = 0; i < weights.length; i++){
+        for(let j = 0; j < weights.length; j++){
+            // console.log(weights[i] + ' vs ' + weights[j] + ' => ' + h2h[i].slice(j, j + 1))
+            if(h2h[i].slice(j, j + 1) === 'W'){
+                // win
+                count[i]++;
+                if(weights[i] < weights[j]){
+                // 몸무게가 큰 이긴 횟수
+                    count[i]++;
+                    if(count[i] === count[j]) {
+                        console.log(count[i] + ' + ' + count[j]);
+                    }
+                }
+            }
+        }
+    }
+    const obj = Object.assign({}, [...count]);
+
+    const arrObj = Object.entries(obj).sort((a, b) => {
+        if(a[a.length - 1] > b[b.length - 1]) return -1
+        if(a[a.length - 1] < b[b.length - 1]) return 1
+        if(a[a.length - 1] == b[b.length - 1]) return 0
+    });
+
+    for(let i = 0; i < weights.length; i++){
+        for(let j = 0; j < weights.length; j++){
+            if(
+                (arrObj[i][1] === arrObj[j][1]) 
+                && (i !== j) 
+                && (weights[arrObj[i][0]] > weights[arrObj[j][0]])
+            ){
+                const tmp = arrObj[i];
+                arrObj[i] = arrObj[j];
+                arrObj[j] = tmp
+            }
+        }
+    }
+
+    return arrObj.map((cur, idx) => cur = Number(arrObj[idx][0]) + 1);
+}
+```
--- programmers/85002/js.js
+++ programmers/85002/js.js
@@ -10,16 +10,13 @@ function solution(weights, h2h){
                 if(weights[i] < weights[j]){
                 // 몸무게가 큰 이긴 횟수
                     count[i]++;
-                    if(count[i] === count[j]) {
-                        console.log(count[i] + ' + ' + count[j]);
-                    }
                 }
             }
         }
     }
     const obj = Object.assign({}, [...count]);
 
-    const arrToObj = Object.entries(obj).sort((a, b) => {
+    const arrObj = Object.entries(obj).sort((a, b) => {
         if(a[a.length - 1] > b[b.length - 1]) return -1
         if(a[a.length - 1] < b[b.length - 1]) return 1
         if(a[a.length - 1] == b[b.length - 1]) return 0
@@ -28,17 +25,20 @@ function solution(weights, h2h){
     for(let i = 0; i < weights.length; i++){
         for(let j = 0; j < weights.length; j++){
             if(
-                (arrToObj[i][1] === arrToObj[j][1]) 
+                (arrObj[i][1] === arrObj[j][1]) 
                 && (i !== j) 
-                // && (weights[i - 1] > weights[j - 1])
+                && (weights[arrObj[i][0]] > weights[arrObj[j][0]])
             ){
-                console.log(arrToObj[i][1], arrToObj[j][1]);
-                console.log(weights[i - 1], weights[j + 1]);
+                const tmp = arrObj[i];
+                arrObj[i] = arrObj[j];
+                arrObj[j] = tmp
             }
         }
     }
 
-    return arrToObj;
+    return arrObj.map((cur, idx) => cur = Number(arrObj[idx][0]) + 1);
 }
 
 console.log(solution([50,82,75,120], ["NLWL","WNLL","LWNW","WWLN"]));
+console.log(solution([145,92,86], ["NLW","WNL","LWN"]));
+console.log(solution([60,70,60], ["NNN","NNN","NNN"]));
