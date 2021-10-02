1d2e50b94c422046130a58eb5ab542d550f5c2b0
ppotatoG <thgus2270@gmail.com>
2021-10-03 05:33:33 +0900

    정답

--- programmers/12940/README.md
+++ programmers/12940/README.md
@@ -4,4 +4,13 @@
 
 ### 제출한 정답
 ```js
+function solution(a, b) {
+    const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
+    return [gcd(a, b), a * b / gcd(a, b)];
+}
 ```
+
+[유클리드 호제법](https://myjamong.tistory.com/138)
+[참고 포스팅](https://swess.tistory.com/13)
+
+왠만하면 이해하고 풀어보려 했는데 이해보다 암기가 더 빠를 것 같아
--- programmers/12940/js.js
+++ programmers/12940/js.js
@@ -1,14 +1,8 @@
-function solution(n, m) {
-    let g, l;
-
-    l = n*m*g;
-
-    for(let i  = 1; i < m +1; i++){
-        if(n % i == 0 & m % i == 0) g = i; 
-    }
-
-    console.log(g, n*m*g)
+function solution(a, b) {
+    const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
+    return [gcd(a, b), a * b / gcd(a, b)];
 }
 
 console.log(solution(3, 12));
 console.log(solution(2, 5));
+console.log(solution(72, 30));
