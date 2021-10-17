3dbd624dd1a2bf2ed009674f42eb242e2d36c707
ppotatoG <thgus2270@gmail.com>
2021-10-17 22:28:17 +0900

    비교까지 완료

--- programmers/42840/js.js
+++ programmers/42840/js.js
@@ -1,29 +1,44 @@
 function solution(arr) {
-    let obj = {
+    const obj = {
         std1 : [1, 2, 3, 4, 5],
         std2 : [2, 1, 2, 3, 2, 4, 2, 5],
         std3 : [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     }
 
-    let values = Object.values([...obj]);
-
-    console.log(values)
+    const values = Object.values(obj);
 
     for(let i = 0; i < values.length; i++){
-        console.log(values[i])
-
-        const num1 = Math.ceil((arr.length - values[i].length) / values[i].length)
+        const num1 = Math.ceil((arr.length - values[i].length) / values[i].length);
 
         if(values[i].length < arr.length) {
             for(let j = 0; j < num1; j++){
-                // values[i].push(values[i]);
-                console.log(values)
+                values[i].push(Object.values(obj)[i]);;
             }
         }
     }
 
-    return values;
+    for(let i = 0; i < values.length; i++){
+        values[i] = values[i].flat();
+        for(let k = 0; k < values[i].length; k++){
+            if(typeof(values[i][k]) === 'object'){
+                values[i].splice(k, 1);
+                k--;     
+            }
+        }
+    }
+
+    let count = [0, 0, 0];
+
+    for(let i = 0; i < values.length; i++){
+        for(let k = 0; k < arr.length; k++){
+            if(values[i][k] === arr[k]){
+                count[i] ++;
+            }
+        }
+    }
+
+    return count;
 
 }
-// console.log(solution([1,2,3,4,5]));
-console.log(solution([1,2,3,4,5,1,2,3,4,5,2,3,1,2,3,4,5,1,2,3,4,5,2,3]));
+console.log(solution([1,2,3,4,5]));
+console.log(solution([1,3,2,4,2]));
