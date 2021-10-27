b73de4ba61c591978a80beba104fb27e8518a37f
ppotatoG <thgus2270@gmail.com>
2021-10-27 23:27:51 +0900

    졸려

--- programmers/42862/README.md
+++ programmers/42862/README.md
@@ -31,3 +31,45 @@ function solution(n, lost, reserve) {
     return has;
 }
 ```
+
+배열 오름차순도 했고,,..,.. `lost`, `reserve` 중복도 제외하고 ++도 했다
+
+근데 5, 12만 안돼
+```js
+function solution(n, lost, reserve) {
+    let has = n - lost.length;
+    let arr = [];
+
+    lost.sort((a, b) => a + b);
+    reserve.sort((a, b) => a + b);
+
+    for(let i = 0; i < lost.length; i++){
+        for(let k = 0; k < reserve.length; k++){
+            if(lost[i] === reserve[k]) {
+                reserve.splice(k, 1);
+                has ++;
+            }
+        }
+    }
+
+    for(let i = 0; i < lost.length; i++){
+        arr.push(lost[i] - 1);
+        arr.push(lost[i]);
+        arr.push(lost[i] + 1);
+    }
+
+    for(let i = 0; i < reserve.length; i++){
+        for(let k = 0; k < arr.length; k++){
+            if(reserve[i] === arr[k]) {
+                has++;
+                break;
+            }
+            if(has >= n) break;
+        }
+    }
+    return has;
+}
+```
+[5, 12번 테스트 케이스 문제 발생 시](https://programmers.co.kr/questions/21442)
+
+이거 했잖아.. 내가 한거 아냐..?
--- programmers/42862/js.js
+++ programmers/42862/js.js
@@ -2,9 +2,22 @@ function solution(n, lost, reserve) {
     let has = n - lost.length;
     let arr = [];
 
+    lost.sort((a, b) => a - b);
+    reserve.sort((a, b) => a - b);
+
+    for(let i = 0; i < lost.length; i++){
+        for(let k = 0; k < reserve.length; k++){
+            // console.log(lost[i], reserve[k])
+            if(lost[i] === reserve[k]) {
+                lost.splice(i, 1);
+                reserve.splice(k, 1);
+                has ++;
+            }
+        }
+    }
+
     for(let i = 0; i < lost.length; i++){
         arr.push(lost[i] - 1);
-        arr.push(lost[i]);
         arr.push(lost[i] + 1);
     }
 
@@ -19,7 +32,8 @@ function solution(n, lost, reserve) {
     }
     return has;
 }
-console.log(solution(5, [2, 4], [1, 3, 5])); // 5 
-console.log(solution(5, [2, 4], [3])); // 4
-console.log(solution(3, [3], [1])); // 2
-console.log(solution(12, [1, 2, 3, 4, 8, 9, 10, 11], [9, 10])); // 6
+// console.log(solution(5, [2, 4], [1, 3, 5])); // 5 
+// console.log(solution(5, [2, 4], [3])); // 4
+// console.log(solution(3, [3], [1])); // 2
+// console.log(solution(12, [1, 2, 3, 4, 8, 9, 10, 11], [9, 10])); // 6
+console.log(solution(10, [5, 4, 3, 2, 1, 15], [15, 3, 1, 2, 5, 4])); // 10
