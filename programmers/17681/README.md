c7936de241a9dc6a103c6ad1be250f2354a190ff
ppotatoG <thgus2270@gmail.com>
2021-11-09 11:31:00 +0900

    정답

--- programmers/17681/README.md
+++ programmers/17681/README.md
@@ -4,4 +4,27 @@
 
 ### 제출한 정답
 ```js
+function solution(n, arr1, arr2) {
+
+    let array = [...arr1];
+
+    for(let i = 0; i < arr1.length; i++){
+        let binary1 = arr1[i].toString(2);
+        let binary2 = arr2[i].toString(2);
+
+        array[i] = (Number(binary1) + Number(binary2) + '');
+
+        if(array[i].length !== n){
+            array[i] = ('0').repeat(n - array[i].length) + array[i];
+        }
+    }
+
+    return array
+    .map((val) => (val + '')
+    .split('')
+    .map((val2) => {
+            return val2 === '0' ? val2 = ' ' : val2 = '#';
+        }
+    ).join(''));
+}
 ```
--- programmers/17681/js.js
+++ programmers/17681/js.js
@@ -1,20 +1,38 @@
 function solution(n, arr1, arr2) {
-    arr1 = arr1.map((val) => {
-        let binary = val.toString(2);
-        return binary.length !== n ? ('0').repeat(n - binary.length) + binary  : binary;
-    });
 
-    arr1 = arr1.map((val) => val.split('').map((val2) => {
-            return val2 === '1' ? val2 = '#' : val2 = ' ';
+    let array = [...arr1];
+
+    for(let i = 0; i < arr1.length; i++){
+        let binary1 = arr1[i].toString(2);
+        let binary2 = arr2[i].toString(2);
+
+        array[i] = (Number(binary1) + Number(binary2) + '');
+
+        if(array[i].length !== n){
+            array[i] = ('0').repeat(n - array[i].length) + array[i];
         }
-    ));
+    }
 
-    return arr1
+    return array
+    .map((val) => (val + '')
+    .split('')
+    .map((val2) => {
+            return val2 === '0' ? val2 = ' ' : val2 = '#';
+        }
+    ).join(''));
 }
-console.log(solution(
-    5,
-    [9, 20, 28, 18, 11],
-    [30, 1, 21, 17, 28]
-)); 
+// console.log(solution(
+//     5,
+//     [9, 20, 28, 18, 11],
+//     [30, 1, 21, 17, 28]
+// )); 
 
 // ["#####","# # #", "### #", "# ##", "#####"]
+
+console.log(solution(
+    6, 
+    [46, 33, 33, 22, 31, 50], 
+    [27, 56, 19, 14, 14, 10]
+)); 
+
+// ["######", "###  #", "##  ##", " #### ", " #####", "### # "]
