36e7cdff9b98204ebd9bf91cc5398c4f6824146c
ppotatoG <thgus2270@gmail.com>
2021-11-20 22:56:18 +0900

    객체 test

--- daily/1120/js.js
+++ daily/1120/js.js
@@ -1,13 +1,47 @@
-// 다른 사람이 만들어 놓은 객체
-const user = {
+const obj = {
     name : '홍길동',
-    lecture : '토익 환급반'
+    age : 21
 };
 
-// 내가 작업
-user.showName = function() {console.log(user.name)};
 
-// 사용자가 접속하면 보는 메세지
-for(let key in user) {
-    console.log(`user ${key} is ${user[key]}`)
+// 객체의 값을 배열로 반환
+console.log(Object.entries(obj))
+/*
+0 ['name', '홍길동']
+1 ['age', 21]
+*/
+
+// 객체의 key 가져오기
+console.log(Object.keys(obj)); 
+// ['name', 'age']
+
+// 객체의 value 가져오기
+console.log(Object.values(obj)); 
+// ['홍길동', 21]
+
+// 객체 탐색
+for(let val in obj) { 
+    console.log(`key : ${val}, value : ${obj[val]}`); 
 }
+/*
+key : name, value : 홍길동
+key : age, value : 21
+*/
+
+// 객체 내 key 탐색
+for(i of Object.keys(obj)) {
+    console.log(i);
+}
+/*
+name
+age
+*/
+
+// 객체 내 value 탐색
+for(i of Object.values(obj)) {
+    console.log(i);
+}
+/*
+홍길동
+21
+*/
