9bf3df145645d544c9a0b00e4e63d7d7805a9081
ppotatoG <thgus2270@gmail.com>
2021-11-29 23:50:12 +0900

    스터디 2주차 과제

--- daily/1129/js.js
+++ daily/1129/js.js
@@ -1,7 +1,14 @@
 let obj = {'start': 1, 'end': 50};
 
-obj.start = obj.start % 2 ? obj.start - 1 : obj.start;
+const arr = new Array(obj.end)
+.fill(obj.start)
+.map((val, idx) => val += idx)
+.filter((a) => a % 2 == 0);
 
+console.log(arr);
+
+
+obj.start = obj.start % 2 ? obj.start - 1 : obj.start;
 const iter = {
     val: obj.start,
     next() {
@@ -15,5 +22,39 @@ const iter = {
     }
 }
 
-console.log(iter)
 console.log([...iter]);
+
+function* gen1(){
+    yield* arr;
+}
+
+console.log([...gen1()])
+
+function* gen2(){
+    for(let i = obj.start; i <= obj.end; i ++){
+        i !== 0 && i % 2 === 0 ? yield i : 0;
+    }
+}
+
+console.log([...gen2()])
+
+function* gen3(startNum){
+    while(startNum <= obj.end){
+        yield startNum
+        startNum += 2;
+    }
+} 
+
+console.log([...gen3(2)])
+
+
+const iterToGen = {
+    *[Symbol.iterator] () {
+        let num = obj.start + 2;
+        while(num <= obj.end){
+            yield num;
+            num += 2;
+        }
+    }
+}
+console.log([...iterToGen])
