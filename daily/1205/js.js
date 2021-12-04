0ffdad87d3a141fa34b7faafd94a6b530133ef81
sohyun1568 <thgus2270@gmail.com>
2021-12-05 04:35:59 +0900

    스터디 예제 풀이

---
+++ daily/1205/js.js
@@ -0,0 +1,48 @@
+class UserStorage {
+    loginUser(id, pw) {
+        return  new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(
+                    (id === 'Id' && pw === 'password') ||
+                    (id === 'hackers' && pw === '1234')
+                ){
+                    resolve(id);
+                }
+                else {
+                    reject(new Error('nor found'));
+                }
+            }, 2000)
+        })
+    }
+
+    getTeam(user) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(user === 'Id') {
+                    resolve({
+                        name : 'Id',
+                        team : 'web development'
+                    });
+                }
+                else if(user === 'hackers') {
+                    resolve({
+                        name : 'hackers',
+                        team : 'module'
+                    });
+                }
+                else {
+                    reject(new Error('no access'));
+                }
+            }, 1000)
+        })
+    }
+}
+
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const password = prompt('enter your password');
+userStorage
+.loginUser(id, password)
+.then(userStorage.getTeam)
+.then(user => alert(`Hello ${user.name}, You're on the ${user.team}team`))
+.catch(console.log);
