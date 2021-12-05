27ecbe3a0875d1ba9ed478cda8a180ffaddb2b71
sohyun1568 <thgus2270@gmail.com>
2021-12-05 22:57:47 +0900

    3주차 예제 풀이

--- study/company/week3/assignment/js.js
+++ study/company/week3/assignment/js.js
@@ -1,36 +1,40 @@
 class UserStorage {
-    loginUser(id, pw, onSuccess, onError) {
-        setTimeout(() => {
-            if(
-                (id === 'Id' && pw === 'password') ||
-                (id === 'hackers' && pw === '1234')
-            ) {
-                onSuccess(id);
-            }
-            else {
-                onError(new Error('not found'));
-            }
-        }, 1000)
+    loginUser(id, pw) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(
+                    (id === 'Id' && pw === 'password') ||
+                    (id === 'hackers' && pw === '1234')
+                ) {
+                    resolve(id);
+                }
+                else {
+                    reject(new Error('not found'));
+                }
+            }, 1000);
+        })
     }
 
-    getTeam(user, onSuccess, onError) {
-        setTimeout(() => {
-            if(user === 'Id') {
-                onSuccess({
-                    name : 'Id',
-                    team : 'web development'
-                });
-            }
-            else if(user === 'hackers') {
-                onSuccess({
-                    name : 'hackers',
-                    team : 'module'
-                });
-            }
-            else {
-                onError(new Error('no access'));
-            }
-        }, 1000)
+    getTeam(user) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(user === 'Id') {
+                    resolve({
+                        name : 'Id',
+                        team : 'web development'
+                    });
+                }
+                else if(user === 'hackers') {
+                    resolve({
+                        name : 'hackers',
+                        team : 'module'
+                    });
+                }
+                else {
+                    reject(new Error('no access'));
+                }
+            }, 1000);
+        })
     }
 }
 
@@ -38,21 +42,8 @@ const userStorage = new UserStorage();
 const id = prompt('enter your id');
 const password = prompt('enter your password');
 
-userStorage.loginUser(
-    id, 
-    password, 
-    user => {
-        userStorage.getTeam(
-            user, 
-            userWidtteam => {
-                alert(`Hello ${userWidtteam.name}, You're on the ${userWidtteam.team}team`)
-            },
-            error => {
-                alert(error);
-            }
-        )
-    },
-    error => {
-        alert(error);
-    }
-)
+userStorage
+.loginUser(id, password)
+.then(userStorage.getTeam)
+.then(user => alert(`Hello ${user.name}, You're on the ${user.team}team`))
+.catch(alert);
