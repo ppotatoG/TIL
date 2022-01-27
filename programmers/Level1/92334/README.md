0e1639cfdd6277fa487ffd1253f62cd4e8284bf5
sohyun1568 <thgus2270@gmail.com>
2022-01-27 23:40:41 +0900

    정답 - 런타임

--- programmers/Level1/92334/README.md
+++ programmers/Level1/92334/README.md
@@ -1,12 +1,37 @@
-# 코딩테스트 나머지가 1이 되는 수 찾기
+# 신고 결과 받기
 
-[programmers](https://programmers.co.kr/learn/courses/30/lessons/87389)
+[programmers](https://programmers.co.kr/learn/courses/30/lessons/92334)
 
 ### 제출한 정답
 ```js
-function solution(num) {
-    for(let i = 2; i < num; i++){
-        if(num % i === 1) return i;
+```
+
+
+### 24개중 7개 런타임
+```js
+const solution = (id_list, report, k) => {
+    report = report
+    .filter((a, b) => report.indexOf(a) === b)
+    .map(val => val.split(' '));
+
+    let answer = id_list.map(val => [val, 0, 0]);
+
+    for(let i = 0; i < report.length; i++) {
+        let findIdx = id_list.findIndex(val => val === report[i][1]);
+        answer[findIdx][1] ++;
     }
+
+    let tmp = answer.filter(val => val[1] >= k).map(val => val[0]);
+    
+    for(let i = 0; i < report.length; i++) {
+        for(let k = 0; k < tmp.length; k++) {
+            if(report[i][1] === tmp[k]) {
+                let findIdx = id_list.findIndex(val => val === report[i][0]);
+                answer[findIdx][2] ++;
+            }
+        }
+    }   
+
+    return answer.map(val => val[2]);
 }
 ```
--- programmers/Level1/92334/js.js
+++ programmers/Level1/92334/js.js
@@ -1,4 +1,4 @@
-function solution(id_list, report, k) {
+const solution = (id_list, report, k) => {
     report = report
     .filter((a, b) => report.indexOf(a) === b)
     .map(val => val.split(' '));
@@ -7,24 +7,21 @@ function solution(id_list, report, k) {
 
     for(let i = 0; i < report.length; i++) {
         let findIdx = id_list.findIndex(val => val === report[i][1]);
-        answer[findIdx][1] += 1;
+        answer[findIdx][1] ++;
     }
 
-    let tmp = answer.filter(val => val[1] >= k);
-
-    console.log(tmp)
-
-    if(tmp) {
-        for(let i = 0; i < report.length; i++) {
-            console.log(report[i][1], report[i][2])
-        }   
-    }
-
-    return answer
-
-    answer = answer.filter(val => val[1] >= k);
+    let tmp = answer.filter(val => val[1] >= k).map(val => val[0]);
     
-    return answer
+    for(let i = 0; i < report.length; i++) {
+        for(let k = 0; k < tmp.length; k++) {
+            if(report[i][1] === tmp[k]) {
+                let findIdx = id_list.findIndex(val => val === report[i][0]);
+                answer[findIdx][2] ++;
+            }
+        }
+    }   
+
+    return answer.map(val => val[2]);
 }
 console.log(solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2)); // [2,1,1,0]
 console.log(solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3)); // [0, 0]
