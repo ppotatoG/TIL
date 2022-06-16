b5940addb892fe138fc91d0bd1ac7287f19af598
sohyun <thgus2270@gmail.com>
2022-06-16 17:52:24 +0900

    .

--- programmers/Level2/62048/js.js
+++ programmers/Level2/62048/js.js
@@ -1,22 +1,6 @@
-function solution(w, h) {
-    const num = Math.ceil(Math.sqrt((h*h) + (w*w)));
-    const sum = w * h
-    
-    // 정사각형
-    if(w === h) {
-        return sum - w;
-    }
-    // 둘 다 홀수
-    else if (w % 2 !== 0 && h % 2 !== 0) {
-        return sum - num + 1
-    }
-    // 둘 다 짝수
-    else if (w % 2 === 0 && h % 2 === 0) {
-        return sum - num - 1;
-    }
-    else {
-        sum - num + 3
-    }
+const solution = (w, h) => {
+    const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
+    return w * h - ( w + h - gcd(w, h));
 }
 
 console.log(solution(8, 12));
