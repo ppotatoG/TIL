09c6285c113aa8d9b5b42a03cae3b8c6d1e96fd9
ppotatoG <thgus2270@gmail.com>
2022-08-24 23:36:10 +0900

    add: child

--- programmers/skill_check_assignments/100/App.js
+++ programmers/skill_check_assignments/100/App.js
@@ -1,28 +1,65 @@
-const getRoot = JSON.parse('[{"id":"1","name":"노란고양이","type":"DIRECTORY","filePath":null,"parent":null},{"id":"3","name":"까만고양이","type":"DIRECTORY","filePath":null,"parent":null},{"id":"10","name":"고등어무늬 고양이","type":"DIRECTORY","filePath":null,"parent":null},{"id":"13","name":"삼색이 고양이","type":"DIRECTORY","filePath":null,"parent":null},{"id":"14","name":"회색고양이","type":"DIRECTORY","filePath":null,"parent":null},{"id":"20","name":"하얀고양이","type":"DIRECTORY","filePath":"/images/20201218_002047.jpg","parent":null}]');
+const Root = JSON.parse('[{"id":"1","name":"노란고양이","type":"DIRECTORY","filePath":null,"parent":null},{"id":"3","name":"까만고양이","type":"DIRECTORY","filePath":null,"parent":null},{"id":"10","name":"고등어무늬 고양이","type":"DIRECTORY","filePath":null,"parent":null},{"id":"13","name":"삼색이 고양이","type":"DIRECTORY","filePath":null,"parent":null},{"id":"14","name":"회색고양이","type":"DIRECTORY","filePath":null,"parent":null},{"id":"20","name":"하얀고양이","type":"DIRECTORY","filePath":"/images/20201218_002047.jpg","parent":null}]');
+const childRoot = {
+    Root1 : JSON.parse('[{"id":"5","name":"2021/04","type":"DIRECTORY","filePath":null,"parent":{"id":"1"}},{"id":"19","name":"물 마시는 사진","type":"FILE","filePath":"/images/a2i.jpg","parent":{"id":"1"}}]'),
+    Root3 : JSON.parse('[{"id":"6","name":"2021/04","type":"DIRECTORY","filePath":null,"parent":{"id":"3"}}]'),
+    Root10 : JSON.parse('[{"id":"11","name":"고등어친구1","type":"FILE","filePath":"/images/2b0.jpg","parent":{"id":"10"}},{"id":"12","name":"고등어친구2","type":"FILE","filePath":"/images/_rWrxmDVv.png","parent":{"id":"10"}}]'),
+    Root13 : JSON.parse('[{"id":"15","name":"2021","type":"DIRECTORY","filePath":null,"parent":{"id":"13"}}]'),
+    Root14 : JSON.parse('[{"id":"18","name":"회색친구","type":"FILE","filePath":"/images/U5nc2yre6.jpg","parent":{"id":"14"}}]'),
+    Root20 : JSON.parse('[{"id":"22","name":"흰둥이","type":"FILE","filePath":"/images/v0-TjU-1H.jpg","parent":{"id":"20"}}]')
+}
 
 // element
-const Breadcrumb = document.querySelector('.Breadcrumb');
 const App = document.querySelector('main.app');
+const Nodes = document.querySelector('.Nodes');
+const Breadcrumb = document.querySelector('.Breadcrumb');
 
 const mainRender = () => {
-    const Nodes = document.createElement('div');
-    Nodes.className = 'Nodes';
-
-    getRoot.map( data => {
+    Root.map( data => {
         const Node = document.createElement('div');
         Node.className = 'Node';
         Node.innerHTML = `<div>DIRECTORY img</div><p>${data.name}</p>`
-        Nodes.appendChild(Node);
+        Node.dataset.id = data.id;
 
-        console.log(data)
+        Nodes.appendChild(Node);
         return '';
-    } )
+    });
 
     App.appendChild(Nodes);
 };
 
+const directoryRender = ( target ) => {
+    const curIdx = target.dataset.id;
+    const curData = childRoot[`Root${curIdx}`];
+
+    Nodes.innerHTML = '';
+
+    const $prev = document.createElement('div');
+    $prev.innerHTML = `<div>이전으로</div>`;
+    Nodes.appendChild($prev);
+
+    const $curName = document.createElement('div');
+    $curName.innerHTML = `<div>${Root.find(v => v.id === curIdx).name}</div>`
+    Breadcrumb.appendChild($curName);
+
+    curData.map( data => {
+        const Node = document.createElement('div');
+        Node.className = 'Node';
+        Node.innerHTML = `<div>${data.type} img</div><p>${data.name}</p>`
+
+        Nodes.appendChild(Node);
+
+        console.log(data.name)
+        return '';
+    });
+}
+
 (() => {
     mainRender();
+
+    const Node = document.querySelectorAll('.Node');
+    Node.forEach(item => {
+        item.addEventListener('click', (e) => directoryRender(e.target));
+    })
 })();
 
 // const $app = document.querySelector('main.app');
