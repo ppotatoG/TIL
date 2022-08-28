82df3812f60af554eb588a26250dcf33fd3002f0
ppotatoG <thgus2270@gmail.com>
2022-08-28 21:00:23 +0900

    'ㅁ'

--- daily/220817/js.js
+++ daily/220817/js.js
@@ -5,20 +5,16 @@ const attachButton = document.querySelector('label[for=file]');
 const attachImg = [];
 
 const filePreview = (input) => {
-    if(attachImg.length >= 4) {
-        alert('3333');
-        return false;
-    }
-
     if (input.files && input.files[0]) {
         const reader = new FileReader();
 
         reader.onload = function (e) {
             attachImg.push(e.target.result);
 
-            const img = document.createElement('img');
-            img.src = `${e.target.result}`;
-            preview.appendChild(img)
+            const div = document.createElement('div');
+            div.style.backgroundImage = `url(${e.target.result})`;
+
+            preview.insertBefore(div, attachButton);
 
             if(attachImg.length === 4) {
                 attachButton.remove();
